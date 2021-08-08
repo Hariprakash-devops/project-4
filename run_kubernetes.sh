d257cda0656769396f38a3416ae7b3f1fe48382b
Hariprakash-devops <87257154+Hariprakash-devops@users.noreply.github.com>
2021-08-08 20:35:06 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,11 +4,11 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath=minorpatch/udacity-cloud-devops-p4
+dockerpath=hariprakashvk/devops1project-4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run app --image=$dockerpath --port=8080
+kubectl run app2 --image=$dockerpath --port=8080
 
 # Step 3:
 # List kubernetes pods
@@ -16,4 +16,6 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward deployment/app 8080:80
+kubectl port-forward pods/app2 8080:80
+
+cmd /k
